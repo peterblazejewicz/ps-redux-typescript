0cafe7780f2643f7bb60819df2c65586b2a3504a
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-01-02 12:34:48 +0100

    Bootstrap 4 integration with SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import './App.css';
-
-const logo = require('./logo.svg');
-
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+      <div className="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p className="lead">
+          Use this document as a way to quickly start any new project.
+          <br /> All you get is this text and a mostly barebones HTML document.
         </p>
       </div>
     );
